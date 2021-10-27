2c846da4d60df20a5f527592319a7a35aaeb89f5
Aikawa Fuyuka <78573412+FykAikawa@users.noreply.github.com>
2021-10-27 11:41:07 +0900

    Update NAdaBelief.py

--- NAdaBelief.py
+++ NAdaBelief.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Nadam optimizer implementation."""
+"""Nadabelief optimizer implementation."""
 
 import tensorflow.compat.v2 as tf
 from keras import backend_config
